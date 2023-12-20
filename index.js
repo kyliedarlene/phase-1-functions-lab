1682190ebe98915c13f77465d582872d4655d5b9
Kylie Clark <kyliedarlene@icloud.com>
2023-12-19 20:43:51 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+    return (Math.abs(42 - pickupLocation));
+}
+
+function distanceFromHqInFeet(pickupLocation) {
+    return 264 * distanceFromHqInBlocks(pickupLocation);
+}
+
+function distanceTravelledInFeet(startingBlock, endingBlock) {
+     return Math.abs(264* (startingBlock - endingBlock));
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    switch (true) {
+        case distance > 2500:
+            return "cannot travel that far";
+            break;
+        case distance <= 400:
+            return 0;
+            break;
+        case distance > 2000:
+            return 25;
+            break;
+        default:
+            return (distance - 400)*.02;
+            break;
+    }
+}
